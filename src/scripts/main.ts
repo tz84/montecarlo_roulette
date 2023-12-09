0b0c300439453bdaa10011fd80d2979d4009df7c
thwim <89816167+thwim@users.noreply.github.com>
2023-12-08 20:21:58 -0600

    Finished all MonteCarlo Simulator methods and initiated classes to run program

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,95 +1,94 @@
 class RouletteGame {
-
     bankroll: number = 1000;
-    // round outcome where true = win and false = lose 
     outcome: boolean = false;
     gameResult: number = 0;
 
-    // min = 1 and max = 38 (chance of winning on color = 18/38)
     playRound(min: number = 1, max: number = 1) {
-        
         this.gameResult = Math.floor(Math.random() * (max - min)) + min;
 
         if (this.gameResult > 18) {
-            this.outcome = false 
+            this.outcome = false;
+        } else if (this.gameResult < 19) {
+            this.outcome = true;
         }
-        else if(this.gameResult < 19) {
-            this.outcome = true 
-        }
-
     }
 }
 
 abstract class BettingStrategy {
+    currentBet: number = 0;
+    initialBet: number = 0;
 
-        // make initial bet a percentage of the bankroll (2-3%)
-        currentBet: number = 0;  // Initialize with a default value
-        initialBet: number = 0;
+    abstract calculateNextBet(outcome: boolean): number;
 
-        abstract calculateNextBet(outcome: boolean): number 
-
-        placeBet() {
-
-        }
+    placeBet() {
+        // Your betting logic goes here if needed
+    }
 }
-class MartingaleStrategy extends BettingStrategy {
 
-  calculateNextBet(outcome:boolean):number {
-    if (outcome === false) {
-      this.currentBet = this.currentBet * 2;
-  } else {
-      this.currentBet = this.initialBet;
-  }
-  return this.currentBet;
-  }
+class MartingaleStrategy extends BettingStrategy {
+    calculateNextBet(outcome: boolean): number {
+        if (outcome === false) {
+            this.currentBet = this.currentBet * 2;
+        } else {
+            this.currentBet = this.initialBet;
+        }
+        return this.currentBet;
+    }
 }
 
 class MonteCarloSimulator {
-
-    maxRounds: number = 100 
-    numSimulations: number = 1000
-
-    //stores round results 
-    simulationResults: {[key: number]: number} = {}
-
-    // stores final mean an avergae dsitributoin values 
-    statistics: {[key: string]: number} = {}
+    maxRounds: number = 100;
+    numSimulations: number = 1000;
+    simulationResults: { [key: number]: number } = {};
+    statistics: { [key: string]: number } = {};
 
     runSimulations(strategy: BettingStrategy, game: RouletteGame) {
-    // must store final balances and update simulation results 
+        for (let i = 0; i < this.numSimulations; i++) {
+            while (game.bankroll > 0 && game.bankroll < 2000) {
+                // Assuming Martingale strategy for simplicity
+                strategy.initialBet = 0.02 * game.bankroll; // 2% of bankroll
+                strategy.currentBet = strategy.initialBet;
+
+                game.playRound();
+                strategy.calculateNextBet(game.outcome);
+                game.bankroll += game.outcome ? strategy.currentBet : -strategy.currentBet;
+            }
+            this.simulationResults[i] = game.bankroll;
+            game.bankroll = 1000; // Reset bankroll for the next simulation
+        }
     }
 
-    
     getSimulationResults() {
         if (Object.values(this.simulationResults).length === 0) {
             throw new Error("Simulation results not available");
         }
-        // returns array of final balances 
+        return Object.values(this.simulationResults);
     }
 
     calculateAndDisplayStatistics(simulationResults: number[]) {
-        //needs values of all final balances in the parameters 
-
-        const finalBalances = Object.values(this.simulationResults)
+        const finalBalances = simulationResults;
 
         if (finalBalances.length == 0) {
-            throw new Error("Simulation results not availbe ")
+            throw new Error("Simulation results not available");
         }
-        
-        // calculate the mean 
-        const sum = finalBalances.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-        const mean = sum / finalBalances.length
 
-        //calculate the standard deviation 
+        const sum = finalBalances.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        const mean = sum / finalBalances.length;
+
         const squaredDifferences = finalBalances.map((balance) => Math.pow(balance - mean, 2));
         const variance = squaredDifferences.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / finalBalances.length;
         const standardDeviation = Math.sqrt(variance);
 
-        //display (later replace with frontend dislay )
         console.log("Mean:", mean);
         console.log("Standard Deviation:", standardDeviation);
     }
 }
 
-// class instantiations 
-// program run 
+// Instantiate classes and run the program
+const game = new RouletteGame();
+const martingaleStrategy = new MartingaleStrategy();
+const simulator = new MonteCarloSimulator();
+
+simulator.runSimulations(martingaleStrategy, game);
+const simulationResults = simulator.getSimulationResults();
+simulator.calculateAndDisplayStatistics(simulationResults);
